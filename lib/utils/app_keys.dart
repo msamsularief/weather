84395159b563fb3334dfc75c41d7b30664a9495c
samy <msamsularief08@gmail.com>
2023-04-04 05:10:28 +0800

    [add] - app keys

---
+++ lib/utils/app_keys.dart
@@ -0,0 +1,6 @@
+class AppKeys {
+  static const String startUpKey = "___startup_key___";
+  static const String homeKey = "___home_key___";
+
+  static const String regionKey = "___region_key___";
+}
