c404a59b5e813968a005210617838b6d27751232
samy <msamsularief08@gmail.com>
2023-04-04 05:19:04 +0800

    [add] - shared preferences keys

---
+++ lib/utils/prefs_keys.dart
@@ -0,0 +1,5 @@
+class PrefsKeys {
+  static const String lastChoosenRegionKey = "___last_choosen_region_key___";
+  static const String listRegionsKey = "___list_region_key___";
+  static const String listRegenciesKey = "___list_regencies_key___";
+}
