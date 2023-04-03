6745dd53f631f07fd36d2ccdbc4ca9f7453e5065
samy <msamsularief08@gmail.com>
2023-04-04 05:24:04 +0800

    [update] - implement app bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:weather/blocs/weather/weather.dart';
-import 'package:weather/screens/weather/weather_page.dart';
 
-// import '../screens/home.dart';
+import 'blocs/app/app_event.dart';
+import 'blocs/app/app_state.dart';
+import 'blocs/weather/weather.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import './blocs/bloc_observer.dart';
-import 'utils/app_getter_name.dart';
+import 'blocs/bloc_observer.dart';
+import 'blocs/app/app_bloc.dart';
+import 'screens/home.dart';
+import 'screens/loading/loading_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +20,16 @@ void main() async {
 
   Bloc.observer = SimpleBlocObserver();
 
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(providers: [
+      BlocProvider(
+        create: (context) => AppBloc()..add(StartUpEvent()),
+      ),
+      BlocProvider(
+        create: (context) => WeatherBloc(),
+      ),
+    ], child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -35,31 +47,48 @@ class MyApp extends StatelessWidget {
         ThemeData.light(
           useMaterial3: true,
         ).copyWith(
-            tabBarTheme: TabBarTheme(
-          indicatorColor: Colors.blue,
-          labelStyle: style.copyWith(
-            fontSize: 18.0,
-            fontWeight: FontWeight.w600,
+          colorScheme: ColorScheme.light(background: Colors.blue.shade50),
+          tabBarTheme: TabBarTheme(
+            indicatorColor: Colors.blue,
+            labelStyle: style.copyWith(
+              fontSize: 18.0,
+              fontWeight: FontWeight.w600,
+            ),
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.blue.shade300,
+            overlayColor: const MaterialStatePropertyAll<Color>(Colors.white),
           ),
-          labelColor: Colors.blue,
-          unselectedLabelColor: Colors.blue.shade300,
-          overlayColor: const MaterialStatePropertyAll<Color>(Colors.white),
-        )),
+        ),
         TextTheme(
           bodySmall: style,
           bodyMedium: style,
           bodyLarge: style,
         ),
       ),
-      home: BlocProvider<WeatherBloc>(
-        create: (context) => WeatherBloc()
-          ..add(
-            GetRegionFromBMKG(
-              AppGetterName.region,
-            ),
-          ),
-        child: const WeatherPage(),
-      ),
+      home: BlocListener<AppBloc, AppState>(
+          listener: (context, state) {
+            if (state is StartUpSuccess) {
+              context.read<WeatherBloc>().add(
+                    GetRegencyFromBMKG(state.choosenRegion.code),
+                  );
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Home(
+                    lastChoosenRegion: state.choosenRegion,
+                    regions: state.regions,
+                  ),
+                ),
+              );
+            } else if (state is StartUpFailure) {
+              context.read<WeatherBloc>().add(GetRegionFromBMKG());
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Home()),
+              );
+            }
+          },
+          child: const LoadingScreen()),
     );
   }
 }
