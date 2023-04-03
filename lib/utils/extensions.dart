7fceaaee188842df9ccf05ab894d1261329748a3
samy <msamsularief08@gmail.com>
2023-04-04 05:05:31 +0800

    [update] - default parser for datetime

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -5,12 +5,22 @@ const String _locale = "id_ID";
 extension ToDate on String {
   String toDDMMMyyyy() {
     DateFormat formater = DateFormat('EEEE, dd MMMM HH:mm', _locale);
-
     int parser = int.parse(this);
     DateTime toFormat = DateTime.fromMillisecondsSinceEpoch(parser);
     String formatted = formater.format(toFormat);
 
     return formatted;
+  }
+
+  String toDefault() {
+    int year = int.parse(substring(0, 4));
+    int month = int.parse(substring(4, 6));
+    int day = int.parse(substring(6, 8));
+    int hour = int.parse(substring(8, 10));
+    int minute = int.parse(substring(10, 12));
+
+    DateTime dateTime = DateTime(year, month, day, hour, minute);
+    return dateTime.toString();
   }
 
   /// Mengambil HH:MM pada DateTime (String)
